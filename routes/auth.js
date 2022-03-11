425929436ca4f0777bcd9a6bec50aba52dba1a91
Joseph Dubon <josephdubon@pm.me>
2022-03-10 17:25:27 -0800

    added logout to routes

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 // controllers
-import {register, login} from '../controllers/auth'
+import {login, logout, register} from '../controllers/auth'
 
 // routes
 router.post('/register', register)
 router.post('/login', login)
+router.get('/logout', logout)
 
 module.exports = router
