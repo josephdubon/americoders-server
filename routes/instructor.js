eb580868ffe4fb73f455925f569840da9232716c
Joseph Dubon <josephdubon@pm.me>
2022-03-25 18:43:27 -0700

    added new route with new controller for currentInstructor

--- routes/instructor.js
+++ routes/instructor.js
@@ -5,10 +5,12 @@ const router = express.Router()
 import {requireSignIn} from '../middlewares'
 
 // controllers
-import {getAccountStatus, makeInstructor} from '../controllers/instructors'
+import {currentInstructor, getAccountStatus, makeInstructor} from '../controllers/instructors'
 
 // routes
 router.post('/make-instructor', requireSignIn, makeInstructor)
 router.post('/get-account-status', requireSignIn, getAccountStatus)
+
+router.get('/current-instructor', requireSignIn, currentInstructor)
 
 module.exports = router
