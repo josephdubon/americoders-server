cd19abbf453398644bfeb7d91b056cb6d1ee1a40
Joseph Dubon <josephdubon@pm.me>
2022-03-26 19:02:14 -0700

    added new route with connected controller for removeImage

--- routes/course.js
+++ routes/course.js
@@ -7,9 +7,10 @@ const router = express.Router()
 import {requireSignIn} from '../middlewares'
 
 // controllers
-import {uploadImage} from '../controllers/courses'
+import {removeImage, uploadImage} from '../controllers/courses'
 
 // routes
 router.post('/course/upload-image', uploadImage)
+router.post('/course/remove-image', removeImage)
 
 module.exports = router
