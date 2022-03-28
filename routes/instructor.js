e77fd1166ffd666ac5d6b96e325cc4bb9db88b48
Joseph Dubon <josephdubon@pm.me>
2022-03-28 13:57:17 -0700

    added new route for instructor courses with requireSignIn and instructorCourses as dependencies

--- routes/instructor.js
+++ routes/instructor.js
@@ -5,12 +5,13 @@ const router = express.Router()
 import {requireSignIn} from '../middlewares'
 
 // controllers
-import {currentInstructor, getAccountStatus, makeInstructor} from '../controllers/instructors'
+import {currentInstructor, getAccountStatus, instructorCourses, makeInstructor} from '../controllers/instructor'
 
 // routes
 router.post('/make-instructor', requireSignIn, makeInstructor)
 router.post('/get-account-status', requireSignIn, getAccountStatus)
 
 router.get('/current-instructor', requireSignIn, currentInstructor)
+router.get('/instructor-courses', requireSignIn, instructorCourses)
 
 module.exports = router
