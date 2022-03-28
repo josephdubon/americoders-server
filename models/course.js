ae38185541dc3e0112df6f7116463a5de7777d3f
Joseph Dubon <josephdubon@pm.me>
2022-03-28 11:21:52 -0700

    added comments

--- models/course.js
+++ models/course.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose'
 
 const {ObjectId} = mongoose.Schema
 
+// lesson schema, multiple lessons can be set to one course
 const lessonSchema = new mongoose.Schema({
         title: {
             type: String,
@@ -27,6 +28,8 @@ const lessonSchema = new mongoose.Schema({
     {timestamps: true} // automatically manage createdAt and updatedAt properties on your documents
 )
 
+
+// course schema, parent to lessonSchema
 const courseSchema = new mongoose.Schema(
     {
         name: {
