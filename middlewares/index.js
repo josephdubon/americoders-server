0943689ab5f342a5b3790fc24e754bd51f63e9df
Joseph Dubon <josephdubon@pm.me>
2022-03-28 12:44:19 -0700

    changed require to import for expressJwt, added linebreak

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,5 +1,8 @@
-const expressJwt = require('express-jwt')
+import expressJwt from 'express-jwt'
+import User from '../models/user'
 
+
+// confirm user is signedIn in to continue request
 export const requireSignIn = expressJwt({
     getToken: (req, res) => req.cookies.token,
     secret: process.env.JWT_SECRET,
