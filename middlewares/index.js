662ef33a6d36870eeabb64b5e05fe5fe4d4a62e2
Joseph Dubon <josephdubon@pm.me>
2022-05-15 16:54:30 -0700

    created new middleware isEnrolled to confirm course enrollment by user

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,5 +1,6 @@
 import expressJwt from 'express-jwt'
 import User from '../models/user'
+import Course from '../models/course'
 
 
 // confirm user is signedIn in to continue request
@@ -24,3 +25,30 @@ export const isInstructor = async (req, res, next) => {
         console.log('IS INSTRUCTOR ', err)
     }
 }
+
+// confirm user is enrolled to view user course
+export const isEnrolled = async (req, res, next) => {
+    try {
+        // get user
+        const user = await User.findById(req.user._id).exec()
+
+        // get user courses
+        const course = await Course.findOne({slug: req.params.slug}).exec()
+
+        // check if course_id is found in user courses array
+        let ids = []
+        for (let i = 0; i < user.courses.length; i++) {
+            ids.push(user.courses[i].toString())
+        }
+
+        if (!ids.includes((course._id.toString()))) {
+            res.sendStatus(403)
+        } else {
+            next()
+        }
+
+    } catch (err) {
+        console.log('MIDDLEWARE IS_ENROLLED ERR: ', err)
+    }
+}
+
