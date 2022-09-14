f43bd53211f3cb81f9055279780046df00fff0e5
Joseph Dubon <josephdubon@pm.me>
2022-09-14 14:59:48 -0700

    updated courses schema added eventStartDate added eventEndDate added formatting, fixed indents

--- models/course.js
+++ models/course.js
@@ -42,45 +42,54 @@ const lessonSchema = new mongoose.Schema({
 
 // course schema, parent to lessonSchema
 const courseSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            trim: true,
-            minlength: 3,
-            maxlength: 320,
-            required: true,
-        },
-        slug: {
-            type: String,
-            lowercase: true,
-        },
-        description: {
-            type: {},
-            minlength: 200,
-            required: true
-        },
-        price: {
-            type: Number,
-            default: 49.99,
-        },
-        image: {},
-        category: String,
-        published: {
-            type: Boolean,
-            default: false,
-        },
-        paid: {
-            type: Boolean,
-            default: true,
-        },
-        instructor: { // make relationship instructor -> lessons
-            type: ObjectId,
-            ref: 'User',
-            required: true,
-        },
-        lessons: [lessonSchema] // each lesson in array must meet requirements set from lesson schema
+  {
+    name: {
+      type: String,
+      trim: true,
+      minlength: 3,
+      maxlength: 320,
+      required: true,
     },
-    {timestamps: true} // automatically manage createdAt and updatedAt properties on your documents
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    description: {
+      type: {},
+      minlength: 200,
+      required: true,
+    },
+    eventStartDate: {
+      type: Date,
+      required: true,
+    },
+    eventEndDate: {
+      type: Date,
+      required: true,
+      default: new Date('2033'),
+    },
+    price: {
+      type: Number,
+      default: 49.99,
+    },
+    image: {},
+    category: String,
+    published: {
+      type: Boolean,
+      default: false,
+    },
+    paid: {
+      type: Boolean,
+      default: true,
+    },
+    instructor: { // make relationship instructor -> lessons
+      type: ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    lessons: [lessonSchema] // each lesson in array must meet requirements set from lesson schema
+  },
+  { timestamps: true } // automatically manage createdAt and updatedAt properties on your documents
 )
 
 // create the schema into a model
